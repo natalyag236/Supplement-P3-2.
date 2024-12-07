b7bb911ba170fc9364fc6998541a106d332e6a2a
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-12-06 22:49:22 -0500

    test: Add test cases for getNumOfCPUs function

--- getMemoryCheck.test.js
+++ getMemoryCheck.test.js
@@ -1,13 +1,12 @@
+
 const { expect } = require('chai');
 const { hasEnoughMemory } = require('./getMemoryCheck');
-
 describe('hasEnoughMemory', () => {
     it('should return true if the system has enough memory to allocate the specified amount', () => {
         const allocationMB = 100; 
         const result = hasEnoughMemory(allocationMB);
         expect(result).to.be.true;
     });
-
     it('should return false if the system does not have enough memory to allocate the specified amount', () => {
         const allocationMB = Number.MAX_SAFE_INTEGER; 
         const result = hasEnoughMemory(allocationMB);
