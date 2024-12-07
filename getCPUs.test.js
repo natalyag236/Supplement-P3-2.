8f0374dbafc33a938bfbed3bc7d82cd136ccf020
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-12-06 21:12:13 -0500

    test: updated  test case for getNumOfCPUs function

--- getCPUs.test.js
+++ getCPUs.test.js
@@ -1,9 +1,11 @@
-const getNumberOfCPUs = require('./getCPUs');
-const { expect } = require('chai');
+const { getNumOfCPUs } = require('./getCPUs'); 
+const chai = require('chai');
+const expect = chai.expect;
 
 describe('getNumOfCPUs', function() {
-    it('should return the number of CPUs', function() {
-        const cpus = getNumOfCPUs();
-        expect(cpus).to.be.greaterThan(0); 
-    });
+  it('should return the number of CPUs', function() {
+    const numCPUs = getNumOfCPUs();
+    expect(numCPUs).to.be.a('number');
+    expect(numCPUs).to.be.greaterThan(0); 
+  });
 });
