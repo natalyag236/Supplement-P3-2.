803a4ea031d509f14f001fceec56cf7923b928b0
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-12-06 21:22:50 -0500

    doc: add documentation for getNumOfCPUs function

--- getCPUs.js
+++ getCPUs.js
@@ -1,6 +1,9 @@
 const os = require('os');
 
-
+/**
+ * Function to return the number of CPUs on the machine.
+ * @returns {number} The number of CPUs.
+ */
 function getNumOfCPUs() {
   return os.cpus().length;
 }
