be73713947ce76f0db6a5acb0670d4846c7e4e03
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-12-06 22:53:22 -0500

    docs: Add documentation for getNumOfCPUs

--- getMemoryCheck.js
+++ getMemoryCheck.js
@@ -1,5 +1,9 @@
 const os = require('os');
-
+/**
+ * Function to determine whether the system has enough memory to allocate the specified amount in MB.
+ * @param {number} megabytes - The number of megabytes to check for availability.
+ * @returns {boolean} True if enough memory is available, false otherwise.
+ */
 function hasEnoughMemory(megabytes) {
     const totalMemory = os.totalmem();
     const requiredBytes = megabytes * 1024 * 1024; 
